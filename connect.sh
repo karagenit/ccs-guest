d14ee7e35429d57142bd9ff9478f9dafa6284d44
Caleb Smith <karagenit@outlook.com>
2017-08-25 13:22:58 -0400

    Add End URL Variable

--- connect.sh
+++ connect.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 SSID='CCS_Guest'
-REQ_URL='https://securelogin.hpe.com/swarm.cgi?opcode=cp_generate&orig_url=687474703a2f2f74656368686f756e64732e636f6d2f'
+END_URL='http://techhounds.com/'
+END_URL=`echo -n $END_URL | xxd -p | tr -d '\n'`
+REQ_URL="https://securelogin.hpe.com/swarm.cgi?opcode=cp_generate&orig_url=$END_URL"
 ACK_URL='https://securelogin.hpe.com/swarm.cgi'
-ACK_DATA='orig_url=687474703a2f2f74656368686f756e64732e636f6d2f&opcode=cp_ack'
+ACK_DATA="orig_url=$END_URL&opcode=cp_ack"
 
 $(iwconfig | grep -q "$SSID")
 
